120b83446f55c7d9c6e20a1981b451b4c4b222a3
Jaison A <jaison2005k@gmail.com>
2026-02-26 15:38:32 +0530

    Initial commit

---
+++ src/NotFound.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (  
+        <div className="not-found">
+            <h2>Sorry</h2>
+            <p>The page you are looking for does not exist</p>
+        </div>
+    );
+}
+ 
+export default NotFound;
